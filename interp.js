69a013a08e1a2b7b3d2bbc07847878a9119d86d1
Noé Falzon <1331434+Castux@users.noreply.github.com>
2021-10-20 13:10:56 +0300

    Started color interpolation

---
+++ interp.js
@@ -0,0 +1,91 @@
+const gridSize = 12;
+var cells = [];
+
+function hexToRgb(hex)
+{
+  var aRgbHex = hex.match(/[a-f\d]{1,2}/g);
+  var aRgb = [
+      parseInt(aRgbHex[0], 16),
+      parseInt(aRgbHex[1], 16),
+      parseInt(aRgbHex[2], 16)
+  ];
+  return aRgb;
+}
+
+function rgbToCss(rgb)
+{
+  return `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
+}
+
+function interpolate(a, b, x)
+{
+  return [
+    a[0] * (1-x) + b[0] * x,
+    a[1] * (1-x) + b[1] * x,
+    a[2] * (1-x) + b[2] * x,
+  ];
+}
+
+function setupDivs()
+{
+    var container = document.getElementById("color-palette");
+    container.innerHtml = "";
+
+    var row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+
+    cells = [];
+
+    for (var i = 0 ; i < gridSize ; i++)
+    {
+      var cell = document.createElement("div");
+      cell.classList.add("col");
+      cell.classList.add("cell-" + i);
+      cell.classList.add("cell");
+      row.appendChild(cell);
+      cells.push(cell);
+    }
+}
+
+function drawPalette()
+{
+  var count = document.getElementById("numpoints").value;
+  var colors = [];
+  for(var i = 1; i <= count; i++)
+  {
+    var input = document.getElementById("color" + i);
+    colors[i - 1] = hexToRgb(input.value);
+  }
+
+  cells.forEach((cell, i) =>
+  {
+    var col = interpolate(colors[0], colors[1], i / cells.length);
+    cell.style.backgroundColor = rgbToCss(col);
+  });
+}
+
+function resetNumber()
+{
+  var count = document.getElementById("numpoints").value;
+
+  for(var i = 1; i <= 4; i++)
+  {
+    var input = document.getElementById("color" + i);
+    input.style.display = i <= count ? "" : "none";
+  }
+
+  setupDivs();
+  drawPalette();
+}
+
+function setup()
+{
+  document.getElementById("numpoints").onchange = resetNumber;
+  for (let selector of document.getElementsByClassName("color-selector"))
+  {
+    selector.oninput = drawPalette;
+  }
+
+  resetNumber();
+}
